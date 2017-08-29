8b2401d3f57b11f9f8de944c5ea3887f336dbe6d
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2017-08-29 14:28:39 +0200

    ID translations in BioGateWay

--- lib/rbbt/tasks/FNL/biogateway.rb
+++ lib/rbbt/tasks/FNL/biogateway.rb
@@ -3,11 +3,84 @@ module FNL
   task :biogateway => :tsv do
     tsv = step(:sentence_coverage_NER).load
 
+    log :prepare, "Preparing tsv for biogateway translation"
+    tsv = tsv.to_double
+
+    log :fixing, "Fixing complexes and families"
+    tsv.process "Transcription Factor (Associated Gene Name)" do |name|
+      name = name.first
+      case name
+      when "AP1"
+        AP1_SYN
+      when "NFKB"
+        NFKB_SYN
+      else
+        [name]
+      end 
+    end
+
+    tsv.process "Target Gene (Associated Gene Name)" do |name|
+      name = name.first
+      case name
+      when "AP1"
+        AP1_SYN
+      when "NFKB"
+        NFKB_SYN
+      else
+        [name]
+      end 
+    end
+
+
+    log :translating, "Translating TF"
     tsv = tsv.swap_id "Transcription Factor (Associated Gene Name)", "UniProt/SwissProt Accession"
     tsv.rename_field "UniProt/SwissProt Accession", "Transcription Factor (UniProt/SwissProt Accession)"
 
+    log :translating, "Translating TG"
     tsv = tsv.swap_id "Target Gene (Associated Gene Name)", "Entrez Gene ID"
     tsv.rename_field "Entrez Gene ID", "Target Gene (Entrez Gene ID)"
+
+    tsv
+  end
+
+  dep :biogateway
+  task :biogateway_ids => :tsv do
+    tsv = step(:biogateway).load
+
+    ids =<<-EOF.split("\n")
+Associated Gene Name
+UniProt/SwissProt Accession
+Entrez Gene ID
+Ensembl Gene ID
+Ensembl Protein ID
+RefSeq ID
+RefSeq mRNA
+RefSeq Protein ID
+EOF
+     
+    organism = FNL.organism
+
+    ids.each do |id|
+      log :add_id, id
+      if id != "UniProt/SwissProt Accession"
+        index = Organism.identifiers(organism).tsv :key_field => "UniProt/SwissProt Accession", :fields => [id], :type => :flat, :persist => true
+
+        tsv.add_field "Transcription Factor (#{ id })" do |k,values|
+          names = values["Transcription Factor (UniProt/SwissProt Accession)"]
+          nids = index.values_at(*names).compact.flatten
+          nids
+        end
+      end
+
+      if id != "Entrez Gene ID"
+        index = Organism.identifiers(organism).tsv :key_field => "Entrez Gene ID", :fields => [id], :type => :flat, :persist => true
+        tsv.add_field "Target Gene (#{ id })" do |k,values|
+          names = values["Target Gene (Entrez Gene ID)"]
+          nids = index.values_at(*names).compact.flatten
+          nids
+        end
+      end
+    end
 
     tsv
   end
