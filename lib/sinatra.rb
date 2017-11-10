fbd05b9a3e405f6fa76016c074077fe7366cdbf6
Miguel Vazquez <miguel.vazquez@ntnu.no>
2017-11-10 12:58:37 +0100

    nil

--- lib/sinatra.rb
+++ lib/sinatra.rb
@@ -3,7 +3,7 @@ Workflow.require_workflow "Graph"
 Workflow.require_workflow "Genomics"
 add_workflow Graph, true
 
-Genomics.knowledge_base.register :TF, Rbbt.data.TF, :source => "TF Associated Gene Name=~Associated Gene Name", :target => "TG Associated Gene Name=~Associated Gene Name", :fields => ["Sentence", "Sentence score", "Interaction score"]
+Genomics.knowledge_base.register :TF, Rbbt.root.data.TF, :source => "TF Associated Gene Name=~Associated Gene Name", :target => "TG Associated Gene Name=~Associated Gene Name", :fields => ["Sentence", "Sentence score", "Interaction score"]
 
 
 require 'helpers/graph'
